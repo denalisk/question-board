5f7425522397d86dce752485e33cd5de04b606e2
Sam Kirsch <samdkirsch@gmail.com>
2017-03-24 10:39:08 -0700

    populate answer-tile begin populating new-answer begin linking funcitonality

--- app/components/answer-question.js
+++ app/components/answer-question.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        body: this.get('body'),
+        score: "0",
+        creation_date: moment().format("MM/DD/YYYY"),
+        question: this.get('question')
+      };
+      this.sendAction('saveAnswer', params)
+    }
+  }
 });
