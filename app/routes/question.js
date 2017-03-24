d1ce9cff63e19c6ebbf8eed23c6a7b4485f83876
Sam Kirsch <samdkirsch@gmail.com>
2017-03-24 09:53:49 -0700

    fix errors with linking to question page

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return Ember.RSVP.hash({
-      questions: this.store.findRecord('question', params.question_id)
-    });
+    return this.store.findRecord('question', params.question_id);
   }
 });
