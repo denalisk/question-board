0846fc49cb7e3e7c3a3bfbe82b77f501974aa393
Sam Kirsch <samdkirsch@gmail.com>
2017-03-24 09:09:07 -0700

    define the models in the model files

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  body: DS.attr(),
+  author: DS.attr(),
+  score: DS.attr(),
+  creation_date: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  body: DS.attr(),
+  author: DS.attr(),
+  notes: DS.attr(),
+  score: DS.attr(),
+  status: DS.attr(),
+  answers: DS.hasMany('answer', {async: true}),
+  creation_date: DS.attr()
 });
