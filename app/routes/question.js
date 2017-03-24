14eef96e313ec10589256837a8b510711861662d
Sam Kirsch <samdkirsch@gmail.com>
2017-03-24 09:43:09 -0700

    create preliminary link between question-tile and the question route

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      questions: this.store.findRecord('question', params.question_id)
+    });
+  }
 });
